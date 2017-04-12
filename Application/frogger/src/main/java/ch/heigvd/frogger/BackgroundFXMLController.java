46d7361761a80d818b2528d845c46d502c030d0a
Talpie <tony.clavien@epfl.ch>
2017-04-12 10:42:51 +0200

    Background tests with JavaFx

---
+++ Application/frogger/src/main/java/ch/heigvd/frogger/BackgroundFXMLController.java
@@ -0,0 +1,17 @@
+package ch.heigvd.frogger;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+public class BackgroundFXMLController implements Initializable {
+    
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // Do nothing
+    }    
+}
