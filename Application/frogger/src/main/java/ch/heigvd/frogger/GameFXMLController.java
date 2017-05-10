42319e36386f14bc7c5e1361c22f4f3de2f8195d
Guillaume Milani <milani.guillaume@gmail.com>
2017-05-10 08:31:21 +0200

    Funny looser text

--- Application/frogger/src/main/java/ch/heigvd/frogger/GameFXMLController.java
+++ Application/frogger/src/main/java/ch/heigvd/frogger/GameFXMLController.java
@@ -6,27 +6,46 @@ import ch.heigvd.frogger.item.Player;
 import java.net.URL;
 import java.util.Random;
 import java.util.ResourceBundle;
+
+import javafx.animation.Animation;
+import javafx.animation.Interpolator;
+import javafx.animation.RotateTransition;
+import javafx.animation.ScaleTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Group;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.effect.BlurType;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.util.Duration;
 
 public class GameFXMLController implements Initializable {
 
     @FXML
     private AnchorPane anchorPane;
 
+    private Text looser;
+    int looserSize;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        // Create the canvas
+        Canvas canvas = new Canvas(Constants.GAME_WIDTH, Constants.GAME_HEIGHT);
+        Group elementsGroup = new Group();
+
+        AnchorPane.setTopAnchor(canvas, 0.);
+        anchorPane.getChildren().add(canvas);
+        anchorPane.getChildren().add(elementsGroup);
+
         try {
-            // Create the canvas
-            Canvas canvas = new Canvas(Constants.GAME_WIDTH, Constants.GAME_HEIGHT);
-            Group elementsGroup = new Group();
 
             // Load the background
             Image background = new Image(
@@ -41,10 +60,8 @@ public class GameFXMLController implements Initializable {
 
             // Skier on top of the mountain
             Player player = new Player(14, 5, Constants.ItemType.Skier);
-            
-            Group playerGroup = new Group();
-            playerGroup.getChildren().add(player);
-            
+            elementsGroup.getChildren().add(player);
+
             // Create the two obstacles borders (chalets)
             for (int i = 0; i < Constants.NUM_ROWS; i++) {
                 elementsGroup.getChildren().add(new Obstacle(0, i, Constants.ItemType.Chalet));
@@ -86,16 +103,11 @@ public class GameFXMLController implements Initializable {
             // make the canvas focusable
             canvas.setFocusTraversable(true);
 
-            AnchorPane.setTopAnchor(canvas, 0.);
-            anchorPane.getChildren().add(canvas);
-            anchorPane.getChildren().add(elementsGroup);
-            anchorPane.getChildren().add(playerGroup);
-                    
             // keyboard handler
             canvas.addEventHandler(KeyEvent.KEY_PRESSED, (KeyEvent event) -> {
                 if (Constants.ACTION_ATTACK.containsKey(event.getCode())) {
                     System.out.println("Attacker's action : " + Constants.ACTION_ATTACK.get(event.getCode()) + " on " + event.getCode());
-                    
+
                     try {
                         switch (event.getCode()) {
                             case LEFT:
@@ -114,14 +126,49 @@ public class GameFXMLController implements Initializable {
                                 break;
                         }
                     } catch (CellAlreadyOccupiedException e) {
-                        e.printStackTrace();
+                        cellAlreadyOccupied();
                     }
                 } else if (Constants.ACTION_DEFEND.containsKey(event.getCode())) {
                     System.out.println("Defender's action : " + Constants.ACTION_DEFEND.get(event.getCode()) + " on " + event.getCode());
                 }
             });
-        } catch (CellAlreadyOccupiedException e2) {
-            e2.printStackTrace();
+        } catch (CellAlreadyOccupiedException e) {
+            cellAlreadyOccupied();
+        } catch (Exception e) {
+
         }
+    }
+
+    private void cellAlreadyOccupied() {
+
+        if (looser == null) {
+            looser = new Text("PERDANT");
+            looser.setFill(Color.RED);
+            final DropShadow dropShadow = new DropShadow(BlurType.THREE_PASS_BOX, Color.YELLOW, 20, 0, 10, 10);
+            looser.setEffect(dropShadow);
+            final DropShadow ds2 = new DropShadow(BlurType.THREE_PASS_BOX, Color.BLUE, 20, 0, 10, 10);
+            dropShadow.setInput(ds2);
+            anchorPane.getChildren().add(looser);
+            looser.setTextAlignment(TextAlignment.CENTER);
+            RotateTransition rt = new RotateTransition(Duration.millis(1000), looser);
+            rt.setByAngle(360);
+            rt.setCycleCount(Animation.INDEFINITE);
+            rt.setAutoReverse(false);
+            rt.setInterpolator(Interpolator.LINEAR);
+
+            rt.play();
+            ScaleTransition st = new ScaleTransition(Duration.millis(3000), looser);
+            st.setCycleCount(Animation.INDEFINITE);
+            st.setByX(4);
+            st.setByY(4);
+            st.setAutoReverse(true);
+            st.play();
+        }
+
+        looserSize = 70;
+
+        looser.setFont(new Font(looserSize));
+        looser.setY(Constants.GAME_HEIGHT/2. + looser.getBoundsInLocal().getHeight()/2.);
+        looser.setX(Constants.GAME_WIDTH/2. - looser.getBoundsInLocal().getWidth()/2.);
     }
 }
