699df5b7ab870edec1be96f4f334524c2490ac9c
Talpie <tony.clavien@epfl.ch>
2017-05-17 10:51:30 +0200

    Add Protocol Folder

---
+++ Application/protocol/protocol/src/main/java/ch/heigvd/protocol/App.java
@@ -0,0 +1,13 @@
+package ch.heigvd.protocol;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! I'm the protocol" );
+    }
+}
