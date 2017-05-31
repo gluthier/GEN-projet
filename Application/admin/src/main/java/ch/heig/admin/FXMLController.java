f51767cda940a02cc28ecbf43e98844616de4b20
Maxime Guillod <maxime@proteck.ch>
2017-05-31 11:01:47 +0200

    refactor

--- Application/admin/src/main/java/ch/heig/admin/FXMLController.java
+++ Application/admin/src/main/java/ch/heig/admin/FXMLController.java
@@ -8,6 +8,7 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ProgressIndicator;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
 
@@ -17,7 +18,9 @@ public class FXMLController implements Initializable {
     private static final String SPACE = "      ";
 
     @FXML
-    private GridPane gridLog;
+    private GridPane logGrid;
+    @FXML
+    private ProgressIndicator logProgress;
     private BDD bdd;
 
     public FXMLController() {
@@ -25,19 +28,24 @@ public class FXMLController implements Initializable {
     }
 
     @FXML
-    private void handleButtonAction(ActionEvent event) {
+    private void logUpdate(ActionEvent event) {
+        logProgress.setProgress(0.0f);
+
         // clean grid
-        gridLog.getChildren().clear();
+        logGrid.getChildren().clear();
 
         // update log content
         List<Log> list = bdd.getLog(NB_LOG);
         for (int i = 0; i < list.size(); i++) {
-            gridLog.add(new Text(list.get(i).getClasse() + SPACE), 0, i);
-            gridLog.add(new Text(list.get(i).getUid()+ SPACE), 1, i);
-            gridLog.add(new Text(list.get(i).getType() + SPACE), 2, i);
-            gridLog.add(new Text(list.get(i).getContent() + SPACE), 3, i);
-            gridLog.add(new Text(list.get(i).getDate() + SPACE), 4, i);
+            logGrid.add(new Text(list.get(i).getClasse() + SPACE), 0, i);
+            logGrid.add(new Text(list.get(i).getUid() + SPACE), 1, i);
+            logGrid.add(new Text(list.get(i).getType() + SPACE), 2, i);
+            logGrid.add(new Text(list.get(i).getContent() + SPACE), 3, i);
+            logGrid.add(new Text(list.get(i).getDate() + SPACE), 4, i);
+
+            logProgress.setProgress(i / list.size());
         }
+        logProgress.setProgress(1);
     }
 
     @Override
